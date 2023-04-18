7d4adc6bdc08498defbb043e188bad7dc7f09966
Adrià <adria.lop.mar@gmail.com>
2023-04-18 11:15:51 +0200

    updated CORS for deploy

--- src/main/java/com/adrilopmar/projectsAPI/controller/impl/EmailController.java
+++ src/main/java/com/adrilopmar/projectsAPI/controller/impl/EmailController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/mail")
-@CrossOrigin()
 public class EmailController {
     @Autowired
     EmailServiceImpl emailService;
